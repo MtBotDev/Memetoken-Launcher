ad3caefa82a7c69b0fc2370da633be30af69ccd0
Egiwpd <169078250+black-rustx@users.noreply.github.com>
2024-11-28 00:59:58 +0900

    Update logger.ts

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,19 +1,6 @@
 import { createLogger, transports, format } from 'winston';
 
-export const logger = createLogger({
-	transports: [new transports.Console()],
-	format: format.combine(
-		format.colorize(),
-		format.timestamp(),
-		format.printf(({ timestamp, level, message, meta }) => {
-			let metaStr = '';
-			if (meta) {
-				metaStr = typeof meta === 'object' ? JSON.stringify(meta) : String(meta);
-			}
-			return `[${timestamp}] ${level}: ${message} ${metaStr}`;
-		})
-	),
-});
+export const logger = createLogger();
 
 export const setLogLevel = (logLevel: string) => {
 	logger.level = logLevel;
