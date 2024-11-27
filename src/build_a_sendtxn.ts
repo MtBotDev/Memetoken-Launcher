93cedff5ac8f2c6781ca3cc8884659ccb4f9d4cd
Egiwpd <169078250+black-rustx@users.noreply.github.com>
2024-11-28 01:02:20 +0900

    Update build_a_sendtxn.ts

--- src/build_a_sendtxn.ts
+++ src/build_a_sendtxn.ts
@@ -24,31 +24,14 @@ export async function sendTx(
   txs: (VersionedTransaction | Transaction)[],
   options?: SendOptions
 ): Promise<string[]> {
-  const txids: string[] = [];
-  for (const iTx of txs) {
-    if (iTx instanceof VersionedTransaction) {
-      iTx.sign([payer]);
-      txids.push(await connection.sendTransaction(iTx, options));
-    } else {
-      txids.push(await connection.sendTransaction(iTx, [payer], options));
-    }
-  }
-  return txids;
+  
 }
 
 export async function buildAndSendTx(
   keypair: Keypair,
   innerSimpleV0Transaction: InnerSimpleV0Transaction[],
 ) {
-  const willSendTx = await buildSimpleTransaction({
-    connection,
-    makeTxVersion: TxVersion.V0,
-    payer: keypair.publicKey,
-    innerTransactions: innerSimpleV0Transaction,
-    addLookupTableInfo: cluster == "devnet" ? undefined : LOOKUP_TABLE_CACHE,
-  });
-
-  return await sendTx(connection, keypair, willSendTx, { skipPreflight: true });
+  
 }
 
 export async function build_swap_buy_instructions(
@@ -59,21 +42,7 @@ export async function build_swap_buy_instructions(
   inputTokenAmount: TokenAmount,
   minAmountOut: TokenAmount
 ) {
-  const { innerTransactions } = await Liquidity.makeSwapInstructionSimple({
-    connection,
-    poolKeys,
-    userKeys: {
-      tokenAccounts: tokenAccountRawInfos_Swap,
-      owner: keypair.publicKey,
-    },
-    amountIn: inputTokenAmount,
-    amountOut: minAmountOut,
-    fixedSide: "in",
-    makeTxVersion: TxVersion.V0,
-    computeBudgetConfig: { microLamports: 300_000, units: 400_000 },
-  });
-
-  return innerTransactions;
+  
 }
 
 export async function build_swap_sell_instructions(
@@ -84,19 +53,5 @@ export async function build_swap_sell_instructions(
   inputTokenAmount: TokenAmount,
   minAmountOut: TokenAmount
 ) {
-  const { innerTransactions } = await Liquidity.makeSwapInstructionSimple({
-    connection,
-    poolKeys,
-    userKeys: {
-      tokenAccounts: tokenAccountRawInfos_Swap,
-      owner: keypair.publicKey,
-    },
-    amountIn: inputTokenAmount,
-    amountOut: minAmountOut,
-    fixedSide: "out",
-    makeTxVersion: TxVersion.V0,
-    computeBudgetConfig: { microLamports: 300_000, units: 400_000 },
-  });
-
-  return innerTransactions;
+  
 }
