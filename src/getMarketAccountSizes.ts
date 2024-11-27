cbdc9b62976c4c6937bb3d3d665a65ac44456156
Egiwpd <169078250+black-rustx@users.noreply.github.com>
2024-11-28 01:06:05 +0900

    Update getMarketAccountSizes.ts

--- src/getMarketAccountSizes.ts
+++ src/getMarketAccountSizes.ts
@@ -20,35 +20,5 @@ export default function useSerumMarketAccountSizes({
     requestQueueLength,
     orderbookLength,
 }: useSerumMarketAccountSizesProps, connection: web3.Connection, programID: web3.PublicKey) {
-    const totalEventQueueSize = calculateTotalAccountSize(
-        eventQueueLength,
-        EVENT_QUEUE_HEADER_SIZE,
-        EVENT_SIZE
-    )
-
-    const totalRequestQueueSize = calculateTotalAccountSize(
-        requestQueueLength,
-        REQUEST_QUEUE_HEADER_SIZE,
-        REQUEST_SIZE
-    )
-
-    const totalOrderbookSize = calculateTotalAccountSize(
-        orderbookLength,
-        ORDERBOOK_HEADER_SIZE,
-        ORDERBOOK_NODE_SIZE
-    )
-    // const useRentExemption = connection.getMinimumBalanceForRentExemption
-    // const marketAccountRent = await useRentExemption(Market.getLayout(programID).span);
-    // const eventQueueRent = await useRentExemption(totalEventQueueSize);
-    // const requestQueueRent = await useRentExemption(totalRequestQueueSize);
-    // const orderbookRent = await useRentExemption(totalOrderbookSize);
-
-    return {
-        // marketRent:
-            // marketAccountRent + eventQueueRent + requestQueueRent + 2 * orderbookRent,
-        marketRent: 0,
-        totalEventQueueSize,
-        totalRequestQueueSize,
-        totalOrderbookSize,
-    };
+  
 }
