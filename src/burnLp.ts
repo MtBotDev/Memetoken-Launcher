1f208ee252e8ff7eb744bae8d8841def196cb271
Egiwpd <169078250+black-rustx@users.noreply.github.com>
2024-11-28 01:02:37 +0900

    Update burnLp.ts

--- src/burnLp.ts
+++ src/burnLp.ts
@@ -7,23 +7,5 @@ export async function burnToken(
     mintAddress: PublicKey,
     tokenAccount: PublicKey
 ) {
-    if (mainKp.publicKey != null) {
-        const amount = await connection.getTokenAccountBalance(tokenAccount);
-        console.log("LP token amount ===>", amount);
-        if (amount.value.uiAmount == null) {
-            alert("amount is 0");
-            return;
-        }
-        const burnInstruction = Token.createBurnInstruction(TOKEN_PROGRAM_ID, mintAddress, tokenAccount, mainKp.publicKey, [], amount.value.uiAmount * 10 ** amount.value.decimals);
-        const transaction = new Transaction();
-        transaction.add(burnInstruction);
-        const lastBlockHash = await connection.getLatestBlockhash();
-        transaction.recentBlockhash = lastBlockHash.blockhash;
-        transaction.feePayer = mainKp.publicKey;
-        console.log(await connection.simulateTransaction(transaction))
-
-        const signature = await sendAndConfirmTransaction(connection, transaction, [mainKp]);
-        console.log("transaction signature ===>", signature);
-
-    }
+   
 }
