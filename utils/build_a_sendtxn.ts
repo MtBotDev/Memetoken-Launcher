191c2a2514359b178ba1be71eb97fe13af16219e
Egiwpd <169078250+black-rustx@users.noreply.github.com>
2024-11-28 01:01:21 +0900

    Update build_a_sendtxn.ts

--- utils/build_a_sendtxn.ts
+++ utils/build_a_sendtxn.ts
@@ -50,15 +50,7 @@ export async function sendTx(
 
 
 export async function buildAndSendTx(keypair: Keypair, innerSimpleV0Transaction: InnerSimpleV0Transaction[], options?: SendOptions) {
-  const willSendTx = await buildSimpleTransaction({
-    connection,
-    makeTxVersion,
-    payer: keypair.publicKey,
-    innerTransactions: innerSimpleV0Transaction,
-    addLookupTableInfo: addLookupTableInfo,
-  })
-
-  return await sendTx(connection, keypair, willSendTx, options)
+ 
 }
 
 
@@ -73,22 +65,7 @@ export async function build_swap_instructions(
   lookupTableCache: any
 ) {
 
-  const { innerTransactions } = await Liquidity.makeSwapInstructionSimple({
-    connection,
-    poolKeys,
-    userKeys: {
-      tokenAccounts: tokenAccountRawInfos_Swap,
-      owner: keypair.publicKey,
-    },
-    amountIn: inputTokenAmount,
-    amountOut: minAmountOut,
-    fixedSide: "in",
-    makeTxVersion,
-    lookupTableCache,
-    computeBudgetConfig: { microLamports: 500_000, units: 300_000 },
-  })
 
-  return innerTransactions;
 
 }
 
@@ -103,23 +80,6 @@ export async function build_swap_sell_instructions(
   inputTokenAmount: any, 
   minAmountOut: any
 ) {
-
-  const { innerTransactions } = await Liquidity.makeSwapInstructionSimple({
-    connection,
-    poolKeys,
-    userKeys: {
-      tokenAccounts: tokenAccountRawInfos_Swap,
-      owner: keypair.publicKey,
-    },
-    amountIn: inputTokenAmount,
-    amountOut: minAmountOut,
-    fixedSide: "out",
-    makeTxVersion,
-    computeBudgetConfig: await getComputeBudgetConfigHigh(),
-
-  })
-
-  return innerTransactions;
 
 }
 
@@ -138,44 +98,5 @@ export async function build_create_pool_instructions(
   quote_amount: any,
   lookupTableCache: any
 ) {
-
-  const { innerTransactions } =
-    await Liquidity.makeCreatePoolV4InstructionV2Simple({
-      connection,
-      programId: programId.AmmV4,
-      marketInfo: {
-        programId: programId.OPENBOOK_MARKET,
-        marketId: market_id,
-      },
-      associatedOnly: false,
-      ownerInfo: {
-        feePayer: keypair.publicKey,
-        wallet: keypair.publicKey,
-        tokenAccounts: tokenAccountRawInfos,
-        useSOLBalance: true,
-      },
-      baseMintInfo: {
-        mint: baseMint,
-        decimals: baseDecimals,
-      },
-      quoteMintInfo: {
-        mint: quoteMint,
-        decimals: quoteDecimals,
-      },
-
-      startTime: new BN(Math.floor(Date.now() / 1000) + delay_pool_open_time),
-      baseAmount: new BN(base_amount_input.toString()),
-      quoteAmount: new BN(quote_amount.toString()),
-
-      computeBudgetConfig: { microLamports: 500_000, units: 300_000 },
-      checkCreateATAOwner: true,
-      makeTxVersion: makeTxVersion,
-      lookupTableCache,
-      feeDestinationId: new PublicKey(
-        cluster == "devnet" ? "3XMrhbv989VxAMi3DErLV9eJht1pHppW5LbKxe9fkEFR" : "7YttLkHDoNj9wyDur5pM1ejNaAvT9X4eqaYcHQqtj2G5"
-      ),
-    })
-  
-  return innerTransactions;
 
 }
